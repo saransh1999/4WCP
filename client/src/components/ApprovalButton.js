cc9a32b9b07a4b4dd3de44b3d3503947dee918b3
Saransh <sssaranshss@gmail.com>
2023-02-19 17:50:56 +0530

    created post request on button

--- client/src/components/ApprovalButton.js
+++ client/src/components/ApprovalButton.js
@@ -1,11 +1,44 @@
 import Button from 'react-bootstrap/Button';
+import axios from "axios";
 
-const ApprovalButton = ({ approvalStatus }) => {
+
+const ApprovalButton = ({ approvalStatus , getManagerTimesheetdata,itemId}) => {
+    
+
+    const approvecall = async() =>
+    {
+        const id=itemId;
+        const approvalStatus="Approved";
+        try
+        {
+            const response = await axios.post("http://localhost:5001/Manager",{id,approvalStatus});
+            getManagerTimesheetdata();
+        }
+        catch (err)
+        {
+            console.log(err);
+        }
+    }
+    const denycall = async() =>
+    {
+        const id=itemId;
+        const approvalStatus="Draft";
+        try
+        {
+            const response = await axios.post("http://localhost:5001/Manager",{id,approvalStatus});
+            getManagerTimesheetdata();
+        }
+        catch (err)
+        {
+            console.log(err);
+        }
+    }
+
     if (approvalStatus === "Awaiting Approval") {
         return (
             <>
-                <Button variant="success">Approve</Button>
-                <Button variant="danger">Deny</Button>
+                <Button variant="success" onClick={approvecall}>Approve</Button>
+                <Button variant="danger" onClick={denycall}>Deny</Button>
             </>
         );
     }
@@ -19,7 +52,7 @@ const ApprovalButton = ({ approvalStatus }) => {
     else if (approvalStatus === "Draft") {
         return (
             <>
-                <Button variant="danger">Denied</Button>
+                <Button variant="danger" disabled>Denied</Button>
             </>
         );
     }
