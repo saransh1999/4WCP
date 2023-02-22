63be04bc867938610ed6d1f1bd535d0079586d67
Manraj Singh Hanjra <manraj.hanjra@incedoinc.com>
2023-02-22 19:59:19 +0530

    FixedDB

---
+++ dotnet/LoginAPI2/LoginAPI2/bin/Models/decodemanager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LoginAPI2.Models;
+public partial class decodemanager
+{
+    public string ResourceName { get; set; } = null!;
+
+    public int ResourceId { get; set; }
+
+    public string Passwords { get; set; } = null!;
+
+    public string? IsManager { get; set; }
+}
