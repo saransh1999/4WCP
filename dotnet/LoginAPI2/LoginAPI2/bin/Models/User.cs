0a8e0f8146195692f0dc6615122da3ca17398b4b
saransh1999 <saransh.singh1@incedoinc.com>
2023-02-23 10:33:26 +0530

    added API1

---
+++ dotnet/LoginAPI2/LoginAPI2/bin/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace LoginAPI2.Models
+{
+	public class User
+	{
+		public string Username { get; set; } = string.Empty;
+		public byte[] PasswordHash { get; set; }
+		public byte[] PasswordSalt { get; set; }
+		public string isManager { get; set; } = string.Empty;
+
+		public string accessToken { get; set; } = string.Empty;
+		public string refreshToken { get; set; } = string.Empty;
+		public DateTime TokenCreated { get; set; }
+		public DateTime TokenExpires { get; set; }
+	}
+}
---
+++ dotnet/LoginAPI2/LoginAPI2/bin/Models/UserLogin.cs
@@ -0,0 +1,9 @@
+﻿namespace LoginAPI2.Models
+{
+	public class UserLogin
+	{
+		public string employee_id { get; set; } = string.Empty;
+
+		public string Password { get; set; } = string.Empty;
+	}
+}
